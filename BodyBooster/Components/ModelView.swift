fdc9b4a635768fe0dccaf6fcb634821073535776
Adriano Souza Costa <adriano.souza@prodoctor.net>
2024-01-02 18:41:26 -0300

    Navigate between workout series after end each workout.

---
+++ BodyBooster/Components/ModelView.swift
@@ -0,0 +1,18 @@
+//
+//  ModelView.swift
+//  BodyBooster
+//
+//  Created by ProDoctor on 02/01/24.
+//
+
+import SwiftUI
+
+struct ModelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelView()
+}
---
+++ BodyBooster/Screens/Workout/SetScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SetScreen.swift
+//  BodyBooster
+//
+//  Created by ProDoctor on 02/01/24.
+//
+
+import SwiftUI
+
+struct SetScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetScreen()
+}
